9c5f01baca3c5d5cb3b636e9d6fc6cd342d287fe
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-02-10 23:03:05 -0600

    add transformed 3d models

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import bg from "../../public/background/enchanted_forest.jpeg"
+import RenderModel from "@/components/RenderModel";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25"/>
+      <div className="w-full h-screen">
+      <RenderModel></RenderModel>
+
+      </div>
       
     </main>
   );
---
+++ src/components/RenderModel.jsx
@@ -0,0 +1,14 @@
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+const RenderModel = ({children, className}) => {
+  return (
+    <Canvas className={clsx("w-screen h-screen relative", className)}>
+        <Suspense fallback={null}>
+            {children}
+        </Suspense>
+    </Canvas>
+  )
+}
+
+export default RenderModel
