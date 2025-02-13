abacbe2dfbdcf7fa03a86a4625d552f2a56b9dec
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-02-12 23:12:05 -0600

    initial firefly effect

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import FireFliesBackground from "@/components/FireFliesBackground";
+
 
 const inter = Inter({ subsets: ["latin"],
   variable: "--font-inter"});
@@ -13,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.variable, 'bg-background text-foreground')}>{children}</body>
+      <body className={clsx(inter.variable, 'bg-background text-foreground')}>
+        {children}
+        <FireFliesBackground/>
+      </body>
     </html>
   );
 }
---
+++ src/components/FireFliesBackground.jsx
@@ -0,0 +1,43 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+const createFireFly = () => ({
+  id: Math.random(),
+  top: `${Math.random()*100}%`,
+  left: `${Math.random()*100}%`,
+  animationDuration: `${Math.random()*5 + 5}s`
+});
+
+function FireFliesBackground() {
+  const [fireFiles, setFireFlies] = useState([]);
+
+  useEffect(() => {
+    const addFireflyPeriodically = () => {
+      const newFirefly = createFireFly();
+      setFireFlies(currentFireflies => 
+        [
+          ...currentFireflies,
+          newFirefly
+        ]
+      );
+    };
+
+    const interval = setInterval(addFireflyPeriodically, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='fixed top-0 left-0 w-full h-full -z-10 overflow-hidden'>
+      {
+        fireFiles.map((firefly) => {
+          return <div 
+          key={firefly.id} 
+          className='absolute rounded-full bg-accent w-[20px] h-[20px]' 
+          style={{top: firefly.top, left: firefly.left, animation: `move ${firefly.animationDuration} infinite alternate`}}></div>
+        })
+      }
+    </div>
+  )
+}
+
+export default FireFliesBackground
