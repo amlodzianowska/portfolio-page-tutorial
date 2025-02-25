2ce964cdcc486d2989cdb5c11ca766f66de7b5e2
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-02-24 23:23:41 -0600

    Add toast to email form

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
+import { Toaster, toast } from 'sonner';
 
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
+  
   const sendEmail = (params) => {
+    const toastId = toast.loading("Sending your message, please wait")
     console.log(params);
     emailjs
       .send(
@@ -22,10 +25,10 @@ export default function Form() {
       )
       .then(
         () => {
-          console.log('SUCCESS!');
+          toast.success("I have received your message. I will get back to you soon.", {id: toastId})
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          toast.error("There was an error sending your message. Please reach out agian later.", {id: toastId})
         },
       );
   };
@@ -41,47 +44,50 @@ export default function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}
-    className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
-    >
-      <input 
-        type="text" placeholder="name" 
-        {...register("name", {required: "This field is required", minLength: {value: 3, message: "Name should be at least 3 characters long"}})}
-        className="w-full p-3 rounded-md shadow-ld text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-      />
-      {
-        errors.name && <span className="inline-block self-start text-accent">{errors.name.message}</span>
-      }
+    <>
+      <Toaster richColors={true}/>
+      <form onSubmit={handleSubmit(onSubmit)}
+      className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
+      >
+        <input 
+          type="text" placeholder="name" 
+          {...register("name", {required: "This field is required", minLength: {value: 3, message: "Name should be at least 3 characters long"}})}
+          className="w-full p-3 rounded-md shadow-ld text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+        />
+        {
+          errors.name && <span className="inline-block self-start text-accent">{errors.name.message}</span>
+        }
 
-      <input 
-        type="text" 
-        placeholder="email" 
-        {...register("email", {required: "This field is required", pattern: {value: /^\S+@\S+$/i, message: "Invalid email format"}})} 
-        className="w-full p-3 rounded-md shadow-ld text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-      />
-      {
-        errors.email && <span className="inline-block self-start text-accent">{errors.email.message}</span>
-      }
+        <input 
+          type="text" 
+          placeholder="email" 
+          {...register("email", {required: "This field is required", pattern: {value: /^\S+@\S+$/i, message: "Invalid email format"}})} 
+          className="w-full p-3 rounded-md shadow-ld text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+        />
+        {
+          errors.email && <span className="inline-block self-start text-accent">{errors.email.message}</span>
+        }
 
-      <textarea 
-        placeholder="message" 
-        {...register("message", {required: "This field is required", maxLength: {
-          value: 500,
-          message: "Message should be less than 500 characters"
-        }, minLength: {
-          value: 50,
-          message: "Message should be more than 50 characters"
-        }})} 
-        className="w-full p-3 rounded-md shadow-ld text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-      />
-      {
-        errors.message && <span className="inline-block self-start text-accent">{errors.message.message}</span>
-      }
+        <textarea 
+          placeholder="message" 
+          {...register("message", {required: "This field is required", maxLength: {
+            value: 500,
+            message: "Message should be less than 500 characters"
+          }, minLength: {
+            value: 50,
+            message: "Message should be more than 50 characters"
+          }})} 
+          className="w-full p-3 rounded-md shadow-ld text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+        />
+        {
+          errors.message && <span className="inline-block self-start text-accent">{errors.message.message}</span>
+        }
 
-      <input 
-        value="Cast Your Message!"
-        className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize" type="submit"
-      />
-    </form>
+        <input 
+          value="Cast Your Message!"
+          className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize" type="submit"
+        />
+      </form>
+    </>
   );
 }
