8581d0d3470999d3dfe044b9bd3f90aa2c9051f0
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-02-26 14:44:14 -0600

    make projects page responsive

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -9,8 +9,10 @@ export default function Home() {
   return (
     <>
       <Image src={bg} alt="background-image" className="-z-50 w-full fixed top-0 left-0 h-full object-cover object-center opacity-25"/>
+
       <ProjectsList projects={projectsData}></ProjectsList>
-      <div className="flex items-center justify-center fixed top-20 -left-16 h-screen w-screen-20">
+
+      <div className="flex items-center justify-center fixed top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-16 h-screen w-screen-20">
         <RenderModel>
           <Staff/>
         </RenderModel>
