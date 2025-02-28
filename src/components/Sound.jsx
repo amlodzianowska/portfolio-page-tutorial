6b1245acd7402fe9ca87ffb7285dac1fed4fe8b5
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-02-27 23:40:24 -0600

    toggle volume button

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,24 +1,36 @@
 "use client"
 import React from 'react'
-import { AudioLines } from 'lucide-react'
+import { Volume2, VolumeX } from 'lucide-react'
+import { useState } from 'react'
 
 export const Sound = () => {
-  return (
-    <div className="fixed top-4 right-2.5 xs:right-4 z-50 group">
-        <audio loop>
-            <source src={"/public/audio/birds39-forest-20772.mp3"} type="audio/mpeg"/>
-            Your browser does not support the audio element.
-        </audio>
-        <button 
-            className='text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 left-4 w-fit self-start z-50 ' 
-            aria-label={"sound"} 
-            name={"sound"}>
-            <span className='relative w-14 h-14 p-4 hover:text-accent'>
-                <AudioLines className='w-full h-auto' strokeWidth={1.5}/>
-                <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
-                <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>Sound</span>
-            </span>
-        </button>
-    </div>
-  )
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const toggle = () => {
+        setIsPlaying(!isPlaying)
+    }
+
+    return (
+        <div className="fixed top-4 right-2.5 xs:right-4 z-50 group">
+            <audio loop>
+                <source src={"/public/audio/birds39-forest-20772.mp3"} type="audio/mpeg"/>
+                Your browser does not support the audio element.
+            </audio>
+            <button 
+                className='text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 right-4 w-fit self-start z-50 ' 
+                aria-label={"sound"} 
+                name={"sound"}
+                onClick={toggle}>
+                <span className='relative w-14 h-14 p-4 hover:text-accent'>
+                    {
+                        isPlaying ?
+                        <Volume2 className='w-full h-auto' strokeWidth={1.5}/> :
+                        <VolumeX className='w-full h-auto' strokeWidth={1.5}/>
+                    }
+                    <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
+                    <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>Sound</span>
+                </span>
+            </button>
+        </div>
+    )
 }
