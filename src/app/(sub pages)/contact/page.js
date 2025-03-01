629843ddd030e4562cdba75b91498f6dbbdcd702
Anna Mlodzianowska <90103679+amlodzianowska@users.noreply.github.com>
2025-02-28 19:35:06 -0600

    fix import issue

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png"
-import Form from "@/components/contact/form";
+import Form from "@/components/contact/Form";
 
 export default function Contact() {
   return (
